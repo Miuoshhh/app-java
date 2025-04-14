80cbe1fe1ab29ca03e582c73191b87bab69c3434
Miuoshhh <milymilosz22@gmail.com>
2025-04-14 15:33:19 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Przykład z użyciem tablicy i pętli for-each
+        String[] names = {"Piotr", "Anna", "Jan", "Leszek"};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Lista uczestników zajęć:");
+        for (String name : names) {
+            System.out.println("- " + name);
+        }
+
+        // Można dodać jakąś logikę – np. liczenie liter w imionach
+        System.out.println("\nDługości imion:");
+        for (String name : names) {
+            System.out.println(name + ": " + name.length() + " liter");
         }
     }
 }
